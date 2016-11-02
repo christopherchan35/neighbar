b22241ca29cde74f630b56f21eb3e4a1d39d545e
Christopher Chan <chris@Christophers-MacBook-Pro-2.local>
2016-11-01 17:20:02 -0700

    Able to show all bars in each neighborhood, show specific bar with attributes, and form to create new bar

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -1,2 +1,36 @@
 class BarsController < ApplicationController
+  def index
+    @neighborhood = Neighborhood.find(params[:neighborhood_id])
+    # @bars = Bar.where(neighborhood_id: @neighborhood.id)
+    @bars = @neighborhood.bars
+  end
+
+  def show
+    @bar = Bar.find(params[:id])
+  end
+
+  def new
+    @neighborhood = Neighborhood.find(params[:neighborhood_id])
+    @bar = Bar.new
+  end
+
+  def edit
+  end
+
+  def create
+    @bar.new(bar_params)
+    @neighborhood = Neighborhood.find(params[:neighborhood_id])
+
+    if @bar.save
+      
+    end
+  end
+
+  def update
+  end
+
+  private
+  def bar_params
+    params.require(:bar).permit(:name, :speciality_drink, :cash_only)
+  end
 end
